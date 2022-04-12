2329c7d76f63469b64060b3af54cbd3569ec8b60
Aryeh I. Braid David <aryebdavid@gmail.com>
2022-04-12 20:52:32 -0300

    feat: desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT
+MIN(pl.plan_price) AS faturamento_minimo,
+MAX(pl.plan_price) AS faturamento_maximo,
+ROUND(AVG(p.plan_price), 2) AS faturamento_medio,
+SUM(pl.plan_price) AS faturamento_total
+FROM
+SpotifyClone.plans AS pl
+INNER JOIN
+SpotifyClone.user AS us ON pl.plan_id = us.plan_id;  
